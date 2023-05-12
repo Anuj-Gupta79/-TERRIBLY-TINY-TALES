0c604ff3598ef4105285077393544b05a782acee
Anuj-Gupta79 <anujgupta7906@gmail.com>
2023-05-12 18:55:28 +0530

    completed

--- src/SubmitButtonComponent.js
+++ src/SubmitButtonComponent.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import Histogram from "./Histogram";
 import { useNavigate } from 'react-router-dom';
 
 
@@ -13,27 +12,7 @@ export default function SubmitButtonComponent() {
     navigate('/submit');
   };
 
-//   <Histogram />
   return (
-    // <div>
-    //   {isLoading ? (
-    //     <Histogram />
-      
-    //   ) : (
-    //     <button
-    //       onClick={loadHandle}
-    //       style={{
-    //         margin: "300px",
-    //         padding: "20px",
-    //         width : "30vh",
-    //         borderRadius: "5px",
-    //         cursor: "pointer",
-    //       }}
-    //     >
-    //         <h1>Submit</h1>
-    //     </button>
-    //   )}
-    // </div>
     <button
     onClick={loadHandle}
     style={{
